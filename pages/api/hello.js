7e2c697c9663b6fcd61c003794c7fed1f10e60a8
ASh <89793472+AadraShukla@users.noreply.github.com>
2024-01-31 20:52:44 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  try {
+    const { page } = req.query;
+    const response = await axios.get(`https://www.reddit.com/r/memes.json?after=${page}`);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Error fetching memes:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
