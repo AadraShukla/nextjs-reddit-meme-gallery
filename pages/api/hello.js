0b7e434993a7c67c03059b67d50f7a623315b6fc
ASh <89793472+AadraShukla@users.noreply.github.com>
2024-01-31 22:15:52 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -13,13 +13,8 @@ export default async function handler(req, res) {
  try {
     const { page } = req.query;
     const token = await oauth2.getOAuthAccessToken();
-    const response = await axios.get(`https://www.reddit.com/r/memes.json?after=${page}`, {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'User-Agent': 'nextjs-react-meme-gallery' 
-      }
-    });
-    res.status(200).json(response.data);
+    const response = await oauth2.get(`https://www.reddit.com/r/memes.json?after=${page}`, token, {}, {'User-Agent': 'nextjs-react-meme-gallery'});
+    res.status(200).json(JSON.parse(response));
  } catch (error) {
     console.error('Error fetching memes:', error);
     res.status(500).json({ error: 'Internal Server Error' });
