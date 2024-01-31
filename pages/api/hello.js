ab7783430cb0cdb510217f9942811dd22556e861
ASh <89793472+AadraShukla@users.noreply.github.com>
2024-01-31 21:46:45 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
+import * as oauth from 'oauth';
+
+const oauth2 = new oauth.OAuth2(
+ process.env.JEYiCqBee5kcJ5JZWWNOyA, // obtained from Reddit's app settings
+ process.env.	F48mbkQFpbrAUQujEObgKSMdzwslDQ, // obtained from Reddit's app settings
+ 'https://www.reddit.com/', // base URL
+ null, // authorization path
+ 'https://www.reddit.com/api/v1/access_token' // token path
+);
 
 export default async function handler(req, res) {
  try {
     const { page } = req.query;
+    const token = await oauth2.getOAuthAccessToken();
     const response = await axios.get(`https://www.reddit.com/r/memes.json?after=${page}`, {
       headers: {
-        'User-Agent': 'nextjs-reddit-meme-gallery' 
+        'Authorization': `Bearer ${token}`,
+        'User-Agent': 'your-app-name' // replace 'your-app-name' with the name of your app
       }
     });
     res.status(200).json(response.data);
