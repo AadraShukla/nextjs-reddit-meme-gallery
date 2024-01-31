09c3b8c380f1a7919514286848eb282f32be87bf
ASh <89793472+AadraShukla@users.noreply.github.com>
2024-01-31 21:56:11 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,7 +3,7 @@ import * as oauth from 'oauth';
 
 const oauth2 = new oauth.OAuth2(
  process.env.JEYiCqBee5kcJ5JZWWNOyA, // obtained from Reddit's app settings
- process.env.	F48mbkQFpbrAUQujEObgKSMdzwslDQ, // obtained from Reddit's app settings
+ process.env.F48mbkQFpbrAUQujEObgKSMdzwslDQ, // obtained from Reddit's app settings
  'https://www.reddit.com/', // base URL
  null, // authorization path
  'https://www.reddit.com/api/v1/access_token' // token path
